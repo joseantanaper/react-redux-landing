1d3400ff209505c4971b3ad29250ca210a9d120e
JoseA <joseantanaper@gmail.com>
2023-10-14 18:38:29 +0200

    Middleware

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,15 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
-import counterReducer from "../components/navbar/reducer/counter.slice"
-import themeReducer from "../components/navbar/reducer/theme.slice"
-import clockReducer from "../components/navbar/reducer/clock.slice"
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  applyMiddleware,
+  getDefaultMiddleware,
+} from '@reduxjs/toolkit'
+import { logger } from './middleware'
+
+import counterReducer from '../components/navbar/reducer/counter.slice'
+import themeReducer from '../components/navbar/reducer/theme.slice'
+import clockReducer from '../components/navbar/reducer/clock.slice'
 
 export const store = configureStore({
   reducer: {
@@ -9,6 +17,7 @@ export const store = configureStore({
     theme: themeReducer,
     clock: clockReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
 
 export type AppDispatch = typeof store.dispatch
